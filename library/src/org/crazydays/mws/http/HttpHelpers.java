c0c32e86d5cbbf159a212a0cdc5bd18d83e465f3
Aaron Day <daya@crazydays.org>
2012-10-14 22:02:03 -0400

    Removed unused method

--- library/src/org/crazydays/mws/http/HttpHelpers.java
+++ library/src/org/crazydays/mws/http/HttpHelpers.java
@@ -9,10 +9,6 @@ import org.apache.http.HttpRequest;
 import org.apache.http.ParseException;
 import org.apache.http.util.EntityUtils;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
 public class HttpHelpers
 {
     public static boolean isEntityEnclosingRequest(HttpRequest request)
@@ -24,13 +20,6 @@ public class HttpHelpers
         HttpRequest request)
     {
         return (HttpEntityEnclosingRequest) request;
-    }
-
-    public static JSONObject extractEntityJSON(HttpEntity entity)
-        throws ParseException, JSONException, IOException
-    {
-        return (JSONObject) new JSONTokener(extractEntityContent(entity))
-            .nextValue();
     }
 
     public static HttpEntity extractEntity(HttpRequest request)
